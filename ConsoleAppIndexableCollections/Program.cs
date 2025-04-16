43dfa5ee012c58af7ebc9deb93bc353fc79c3070
Kristian Lubina <kristian.lubina@outlook.com>
2025-04-16 09:59:27 +0200

    Initial commit

---
+++ ConsoleAppIndexableCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿//from here
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/collections#indexable-collections
+
+
+internal class Program
+  {
+  private static void Main( string[] args )
+    {
+    IterateThroughList();
+    }
+
+  private static void IterateThroughList()
+    {
+    var theGalaxies = new List<Galaxy>
+    {
+    new () { Name="Tadpole", MegaLightYears=400},
+    new () { Name="Pinwheel", MegaLightYears=25},
+    new () { Name="Milky Way", MegaLightYears=0},
+    new () { Name="Andromeda", MegaLightYears=3}
+    };
+
+    foreach ( Galaxy theGalaxy in theGalaxies )
+      {
+      Console.WriteLine( theGalaxy.Name + " " + theGalaxy.MegaLightYears );
+      }
+
+    /* 
+     * Output
+     * Tadpole 400
+     * Pinwheel 25
+     * Milky Way 0
+     * Andromeda 3
+     */
+    }
+  }
+
+public class Galaxy
+  {
+  public required string Name { get; set; }
+  public int MegaLightYears { get; set; }
+  }
