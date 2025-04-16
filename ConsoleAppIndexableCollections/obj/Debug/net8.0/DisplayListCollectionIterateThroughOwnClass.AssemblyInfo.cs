e0fdc7485573354b21cffd45fc97db0b16d62123
Kristian Lubina <kristian.lubina@outlook.com>
2025-04-16 14:25:44 +0200

    renaming

--- ConsoleAppIndexableCollections/obj/Debug/net8.0/DisplayListCollectionIterateThroughOwnClass.AssemblyInfo.cs
+++ ConsoleAppIndexableCollections/obj/Debug/net8.0/DisplayListCollectionIterateThroughOwnClass.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CollectionsIterateThroughOwnListClass")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DisplayListCollectionIterateThroughOwnClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe538dc76eeb1071b96a368e9bc3f188bf7c7dd1")]
-[assembly: System.Reflection.AssemblyProductAttribute("CollectionsIterateThroughOwnListClass")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CollectionsIterateThroughOwnListClass")]
+[assembly: System.Reflection.AssemblyProductAttribute("DisplayListCollectionIterateThroughOwnClass")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DisplayListCollectionIterateThroughOwnClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
